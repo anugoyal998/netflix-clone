537f87f680fdc60aab4949b16234a9d002a6eadb
anugoyal998 <anugoyal998@gmail.com>
2021-12-30 16:52:41 +0530

    signup

--- components/home/Navbar.js
+++ components/home/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 import logo from "../../img/netflix-logo-lg.png";
 import { IoTriangleSharp } from "react-icons/io5";
 import { BiWorld } from "react-icons/bi";
-import { signIn } from "next-auth/react";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
@@ -17,14 +17,11 @@ export default function Navbar() {
           <BiWorld /> <p>English</p>{" "}
           <IoTriangleSharp className="rotate-180 text-xs mt-1" />{" "}
         </button>
-        <button
-          className="cursor-pointer outline-none focus:outline-none bg-front border-2 border-front text-white py-1 px-3 font-semibold rounded-md"
-          onClick={() =>
-            signIn(process.env.GOOGLE_ID, { callbackUrl: "/netflix" })
-          }
-        >
-          Sign In
-        </button>
+        <Link href="/login">
+          <a className="cursor-pointer outline-none focus:outline-none bg-front border-2 border-front text-white py-1 px-3 font-semibold rounded-md">
+            Sign In
+          </a>
+        </Link>
       </div>
     </div>
   );
--- pages/login/index.js
+++ pages/login/index.js
@@ -2,17 +2,17 @@ import Image from "next/image";
 import React from "react";
 import Footer from "../../components/footer/Footer";
 import logo from "../../img/netflix-logo-lg.png";
-import { signIn } from "next-auth/react";
+import Link from "next/link";
 
 export default function index() {
   return (
-    <div className="netflix-bg">
+    <div className="netflix-bg h-screen">
       <div className="w-72 h-72 pl-4 pt-5">
         <Image src={logo} alt="" />
       </div>
       <div className="flex justify-center -translate-y-28">
         <div
-          className=" p-10 text-white w-[400px] rounded-md"
+          className=" p-10 text-white w-[400px] rounded-sm"
           style={{ background: "rgba(0,0,0,0.8)" }}
         >
           <p className="text-2xl font-semibold">Sign In</p>
@@ -28,22 +28,10 @@ export default function index() {
               className="py-2 px-4 bg-[#333333] w-full rounded-md text-lg outline-none focus:outline-none"
             />
           </div>
-          <button className="w-full bg-front mt-6 rounded-md py-3 font-semibold text-lg">
+          <button className="w-full bg-front mt-12 rounded-md py-3 font-semibold text-lg">
             Sign In
           </button>
-          <div className="flex items-center justify-center mt-4">
-            <hr className="w-full" />
-            <p className="h-8 w-8 rounded-full border-2 p-3 flex items-center justify-center mx-2 font-semibold">
-              Or
-            </p>
-            <hr className="w-full" />
-          </div>
-          <button
-            className="w-full border-2 mt-5 text-lg font-semibold py-2 rounded-md"
-            onClick={() => signIn(process.env.GOOGLE_ID, { callbackUrl: "/netflix" })}
-          >
-            Continue with Google
-          </button>
+          <p className="mt-4"><span className="opacity-70">New to Netflix ?</span> <Link href="/signup"><a>Sign up now</a></Link></p>
         </div>
       </div>
       <Footer />
